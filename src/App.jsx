6f4e597fbd8d8e76b6ce6f4a52992ad9a1fbc361
Abdelkarim Ben el kadi <aben-el-@c1r7p3.1337.ma>
2025-02-18 22:56:03 +0100

    adding some impliment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./index.css";
 import NavBar from "./components/nav/NavBar";
+import Footer from "./components/footer/footer";
+
 function App() {
 
   return (
     <div>
       <NavBar></NavBar>
+     <Footer></Footer>
     </div>
-  );
+    );
 }
 
 export default App;
--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,20 +1,29 @@
 import style from "./NavBar.module.css";
 import Logo from "../../../public/imges/Untitled_design-removebg-preview.png";
+import { FaBars } from "react-icons/fa6";
+import ResposiveBar from "./Resposive_Bar";
+import { useState } from "react";
 
 function NavBar() {
+	const [showNavBar, setShowNavBar] = useState(false);
+
     return (
-		<div className={style.parent}>
-			<div className={style.navBar}>
-				<a className={style.logo} href="#"> <img src={Logo} alt="" /></a>
-				<ul>
-				<li>Home</li>
-				<li>News</li>
-				<li>Experiences</li>
-				<li>Questions</li>
-				<a className={style.login} href="#">Log in</a>
-				</ul>
-			</div>
+	<>
+	{showNavBar ? <ResposiveBar setShowNavBar={setShowNavBar} />
+	:
+	<div className={style.navBar}>
+			<a className={style.logo} href="#"> <img src={Logo} alt="" /></a>
+			<ul>
+				<li className={style.sideBar}>Home</li>
+				<li className={style.sideBar}>News</li>
+				<li className={style.sideBar}>Experiences</li>
+				<li className={style.sideBar}>Questions</li>
+				<a href="#" className={`${style.sideBar} ${style.login}`} >Log in</a>
+				<FaBars onClick={() => setShowNavBar(true)} className={style.faBars}/>
+			</ul>  
 		</div>
+	}
+	</>
     );
 }
 export default NavBar;
