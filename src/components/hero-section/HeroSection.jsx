776c252772d51f4eaeff9b54c112f3da0c975232
Mohamed Ouahmane <mouahman@c1r7p1.1337.ma>
2025-02-19 10:38:57 +0100

    pushees

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -14,7 +14,7 @@ function HeroSection() {
 	const txt = "discover 1337";
 
 	return (
-		<section className={styles.heroSection}>
+		<section className={styles.heroSection} id="hero-section">
 			<div className={styles.back}></div>
 			<h1>Welcome to 1337++, Your guide for the 1337 school</h1>
 			<a href="#news">
